e19ba1fb1194f77dbf6d6a86787f0604a1c3c158
Andre D <andre@andred.ca>
2014-07-15 18:46:03 -0400

    Initial commit.

---
+++ cutereddit.js
@@ -0,0 +1,29 @@
+var process_subreddit = function(i, data) {
+    if (!data.data.header_img || !data.data.header_size) {
+        return;
+    }
+    $logo = $('<div>')
+    .addClass('header')
+    .css('background-image', 'url("' + data.data.header_img + '")');
+    $sr = $('<a>')
+    .attr('href', 'http://www.reddit.com/r/' + data.data.display_name)
+    .attr('title', data.data.public_description)
+    .append($logo)
+    .addClass('sr')
+    .append($('<span>').text('/r/' + data.data.display_name).addClass('name'))
+    .appendTo($('#content'));
+}
+
+var process_subreddits = function(data) {
+    $('#content').empty();
+    $.each(data.data.children, process_subreddit);
+}
+
+$(function() {
+$.ajax('http://www.reddit.com/subreddits.json?limit=100',
+       {
+           dataType: 'json',
+           success: process_subreddits
+       }
+)
+})
