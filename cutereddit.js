706e5e886d20323224cf413f345741332a495d59
Andre D <andre@andred.ca>
2014-07-15 19:03:25 -0400

    Single line, unescaped title text.

--- cutereddit.js
+++ cutereddit.js
@@ -2,12 +2,14 @@ var process_subreddit = function(i, data) {
     if (!data.data.header_img || !data.data.header_size) {
         return;
     }
+    var sum = data.data.public_description.split('\n')[0]
+    sum = $('<span>').html(sum).text();
     $logo = $('<div>')
     .addClass('header')
     .css('background-image', 'url("' + data.data.header_img + '")');
     $sr = $('<a>')
     .attr('href', 'http://www.reddit.com/r/' + data.data.display_name)
-    .attr('title', data.data.public_description)
+    .attr('title', sum)
     .append($logo)
     .addClass('sr')
     .append($('<span>').text('/r/' + data.data.display_name).addClass('name'))
