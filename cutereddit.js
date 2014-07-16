8797460909acbd73b2538e413f968beb04e30d32
Andre D <andre@andred.ca>
2014-07-16 01:07:08 -0400

    Start of actual functionality.

--- cutereddit.js
+++ cutereddit.js
@@ -1,47 +1,187 @@
-var reddit = 'http://www.reddit.com';
-if (location.protocol == 'https:') {
-    reddit = 'https://pay.reddit.com';
-}
-
-var if_https_rewrite = function(url) {
-    if (location.protocol == 'https:') {
-        return  'https://s3.amazonaws.com/' + url.split('://')[1]
+var CuteReddit = {
+    route: {},
+    init: function() {
+        CuteReddit.SubredditList.init()
+        this.nav(window.location.hash.substring(1))
+        window.onpopstate = $.proxy(this.nav_changed, this)
+    },
+    nav_changed: function() {
+        history.replaceState(undefined, undefined, '#' + this.current_location())
+        if (this.current_nav != this.current_location()) {
+            this.current_nav = this.current_location()
+            if (this.current_nav) {
+                CuteReddit.SubredditList.add_by_name(this.current_nav, true)
+            }
+        }
+    },
+    current_location: function() {
+        var location = window.location.hash.substring(1)
+        return this.Utils.norm_path(location)
+    },
+    nav: function(where) {
+        where = this.Utils.norm_path(where)
+        if (this.current_location() != where) {
+            history.pushState(undefined, undefined, '#' + where)
+        }
+        this.nav_changed()
     }
-    return url;
 }
 
-var process_subreddit = function(i, data) {
-    if (!data.data.header_img || !data.data.header_size) {
-        return;
+CuteReddit.SubredditButton = function(sr) {
+    this.init(sr);
+}
+
+CuteReddit.SubredditButton.prototype = {
+    init: function(sr) {
+        this.sr = sr
+        var tall = false
+        var url = CuteReddit.Utils.make_reddit_url('/static/reddit.com.header.png')
+        if (this.sr.header_img && this.sr.header_size) {
+            url = CuteReddit.Utils.reddit_cdn_url(sr.header_img)
+            tall = this.sr.header_size[1] > this.sr.header_size[0]
+        }
+        var sum = this.sr.public_description.split('\n')[0]
+        sum = $('<span>').html(sum).text()
+        
+        $logo = $('<div>')
+            .addClass('header')
+            .css('background-image', 'url("' + url + '")')
+        if (tall) {
+            $logo.addClass('tall')
+        }
+        this.$el = $('<a>')
+            .attr('href', '#' + sr.url)
+            .attr('title', sum)
+            .append($logo)
+            .addClass('sr')
+            .append($('<span>').text(sr.display_name).addClass('name'))
     }
-    var tall = data.data.header_size[1] > data.data.header_size[0]
-    var sum = data.data.public_description.split('\n')[0]
-    sum = $('<span>').html(sum).text();
-    $logo = $('<div>')
-    .addClass('header')
-    .css('background-image', 'url("' + if_https_rewrite(data.data.header_img) + '")');
-    if (tall) {
-        $logo.addClass('tall');
+}
+
+CuteReddit.ContentView = {
+    init: function(url, context) {
+        this.$el = $('#content')
+        this.$el.addClass('loading')
+        this.context = context
+        CuteReddit.Utils.ajax(
+            url + '.json'
+        ).done($.proxy(this.render_page, this))
+    },
+    add_obj: function(data) {
+        if (data.kind == 't3') {
+            var $link = $('<div>').addClass('link')
+            var $title = $('<div>').addClass('title').appendTo($link)
+            $title.text(data.data.title)
+            $('#content_body').append($link)
+        }
+    },
+    render_page: function(data) {
+        $('#context_header').empty();
+        $('#content_body').empty()
+        $('#context_header').append(new CuteReddit.SubredditButton(this.context).$el)
+        var add = $.proxy(this.add_obj, this)
+        $.each(data.data.children, function(i, data) {add(data)})
+        this.$el.removeClass('loading')
     }
-    $sr = $('<a>')
-    .attr('href', reddit + '/r/' + data.data.display_name)
-    .attr('title', sum)
-    .append($logo)
-    .addClass('sr')
-    .append($('<span>').text('/r/' + data.data.display_name).addClass('name'))
-    .appendTo($('#content'));
 }
 
-var process_subreddits = function(data) {
-    $('#content').empty();
-    $.each(data.data.children, process_subreddit);
+CuteReddit.SubredditList = {
+    initial_complete: 1,
+    subreddits: {},
+    init: function() {
+        this.$el = $('#srlist')
+        CuteReddit.Utils.ajax(
+            'subreddits.json',
+            {data: {limit: 10}}
+        ).done($.proxy(this.init_list, this))
+    },
+    nav_to: function(name) {
+        this.$el.find('li').removeClass('selected')
+        var sr = this.subreddits[name.toLowerCase()]
+        sr.button.$el.parent().addClass('selected')
+        CuteReddit.ContentView.init(sr.url, sr)
+    },
+    add_by_name: function(name, nav_to) {
+        name = name.split('/r/')[1]
+        if (this.subreddits[name.toLowerCase()]) {
+            if (nav_to) {
+                this.nav_to(name)
+            }
+            return
+        }
+        $('#sidebar').addClass('loading')
+        this.initial_complete += 1
+        CuteReddit.Utils.ajax(
+            '/r/' + name + '/about.json'
+        ).done($.proxy(this.add_from_response, this, nav_to))
+    },
+    add_from_response: function(nav_to, data) {
+        this.add(data.data, nav_to)
+        this.initial_complete -= 1
+        if (this.initial_complete <= 0) {
+            $('#sidebar').removeClass('loading')
+        }
+    },
+    add: function(sr, nav_to, initial) {
+        if(!this.subreddits[sr.display_name.toLowerCase()]) {
+            var button = new CuteReddit.SubredditButton(sr)
+            if (initial) {
+                this.$el.append($('<li>').append(button.$el))
+            } else {
+                this.$el.prepend($('<li>').append(button.$el))
+            }
+            sr.button = button
+            this.subreddits[sr.display_name.toLowerCase()] = sr
+        }
+        if (nav_to) {
+            this.nav_to(sr.display_name)
+        }
+    },
+    init_list: function(data) {
+        var add = $.proxy(this.add, this);
+        $.each(data.data.children, function(i, data) {
+            add(data.data, false, true)
+        })
+        $('#sidebar').removeClass('loading')
+        if (!CuteReddit.current_nav) {
+            CuteReddit.nav(data.data.children[0].data.url)
+        }
+        this.initial_complete -= 1
+    }
 }
 
-$(function() {
-$.ajax(reddit + '/subreddits.json?limit=100',
-       {
-           dataType: 'json',
-           success: process_subreddits
-       }
-)
-})
+CuteReddit.Utils = {
+    make_reddit_url: function(url) {
+        var reddit = 'http://www.reddit.com/'
+        if (location.protocol == 'https:') {
+            reddit = 'https://pay.reddit.com/'
+        }
+        return reddit + url
+    },
+    norm_path: function(path) {
+        if (path.substr(-1) == '/') {
+            path = path.substr(0, path.length - 1)
+        }
+        return path
+    },
+    reddit_cdn_url: function(url) {
+        if (location.protocol == 'https:') {
+            return 'https://s3.amazonaws.com/' + url.split('://')[1]
+        }
+        return url
+    },
+    ajax: function(url, options) {
+        var url = this.make_reddit_url(url)
+        return $.ajax(
+            url,
+            $.extend(
+                options,
+                {
+                    dataType: 'json'
+                }
+            )
+        )
+    }
+}
+
+$($.proxy(CuteReddit.init, CuteReddit))
